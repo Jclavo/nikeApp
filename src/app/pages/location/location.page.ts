8a4ecebc6361fe3efe5ffa2cbfc7c124a6f771f1
Jclavo <jclavotafur@gmail.com>
2019-11-13 20:49:47 -0300

    finish location CRUD

--- src/app/pages/location/location.page.ts
+++ src/app/pages/location/location.page.ts
@@ -20,22 +20,30 @@ export class LocationPage implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
+    this.locationService.getAll().subscribe(dataLocations => {
+      //this.loading = false;
+      if (dataLocations) {
+        this.locations = dataLocations;
+      }
+
+      console.log("locations: ", this.locations);
+    });
   }
   
   save(formLocation: NgForm) {
     console.log(formLocation.value);
     this.location.latitude = formLocation.value.latitude;
-    // this.location.latitude = 'formLocation.value.latitude';
     this.location.longitude = formLocation.value.longitude;
     this.location.name = formLocation.value.name;
     this.location.active = formLocation.value.active;
+
+    
     
 
     if (this.location.id) {
       this.locationService.update(this.location).then(() => {
-        //this.router.navigateByUrl('/');
         console.log('Item updated');
-        this.router.navigate(['/home']);
+        formLocation.reset();
         //this.showToast('Idea added');
       }, err => {
         //this.showToast('There was a problem adding your idea :(');
@@ -44,15 +52,35 @@ export class LocationPage implements OnInit {
     }
     else {
       this.locationService.create(this.location).then(() => {
-        this.router.navigateByUrl('/');
         console.log('Item added');
-        this.router.navigate(['/home']);
         //this.showToast('Idea added');
+        formLocation.reset();
       }, err => {
         //this.showToast('There was a problem adding your idea :(');
         console.log('There was a problem adding.....');
       });
     }
+
+    
+    //this.location = new LocationModel('','unknown','','',true);
+  }
+
+  delete(id: string)
+  {
+    this.locationService.delete(id).then(() => {
+      console.log('Location deleted');
+      //this.showToast('Idea added');
+    }, err => {
+      //this.showToast('There was a problem adding your idea :(');
+      console.log('There was a problem deleting.....');
+    });
+  }
+
+  update(id: string)
+  {
+    this.locationService.get(id).subscribe(dataLocation => {
+      this.location = dataLocation;
+    });
   }
   
 }
